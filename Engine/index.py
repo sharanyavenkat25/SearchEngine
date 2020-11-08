d398c0ac1795e8ce243a9801e80bf842d3bd6d80
sharanyavenkat25 <sharan.venkat@gmail.com>
2020-11-08 14:00:11 +0530

    accounted for rowid - final pickle file is token.pkl

--- Engine/index.py
+++ Engine/index.py
@@ -14,14 +14,15 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer 
 
 
-directory = '/mnt/d/SearchEngine/Corpus/'
+directory = '/mnt/d/SearchEngine/Engine/Corpus/'
 
 def clean(df):
 	total_rows = df.shape[0]
 	
-	tokens_without_sw=[]
+	tokens=[]
 	lemmatizer = WordNetLemmatizer()
 	for i in range(total_rows):
+		tokens_without_sw_row=[]
 		document_test = re.sub(r'[^\x00-\x7F]+', ' ', df['text'][i])
 		document_test = re.sub(r'@\w+', '', document_test)
 		document_test = document_test.lower()
@@ -29,9 +30,11 @@ def clean(df):
 		document_test = re.sub(r'[0-9]', '', document_test)
 		document_test = re.sub(r'\s{2,}', ' ', document_test)
 		text_tokens = word_tokenize(document_test)
-		tokens_without_sw.extend([word for word in text_tokens if not word in stopwords.words()])
-	final=[lemmatizer.lemmatize(w) for w in tokens_without_sw]
-	return final
+		tokens_without_sw_row=[word for word in text_tokens if not word in stopwords.words()]
+		lemmatized_tokens_row=[lemmatizer.lemmatize(w) for w in tokens_without_sw_row]
+		tokens.append(lemmatized_tokens_row)
+	# final=[lemmatizer.lemmatize(w) for w in tokens_without_sw]
+	return tokens
 
 def extract(directory):
 
@@ -45,10 +48,18 @@ def extract(directory):
 
 docs = extract(directory)
 
-with open('docs_token_list.pkl','wb') as f:
+
+with open('token.pkl','wb') as f:
 	pickle.dump(docs, f)
 
 print(np.shape(docs))
+print("Number of rows in doc 5", len(docs[5]))
+print("Number of rows in doc 10",len(docs[10]))
+print(docs[0][0])
+print(docs[4][1])
+print(docs[1][5])
+print(docs[5][3])
+
 
 
 
